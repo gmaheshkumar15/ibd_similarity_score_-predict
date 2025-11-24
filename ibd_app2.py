30bccc6b9d61014f88359e5107c43d5d8d221ab6
gmaheshkumar15 <gmaheshk85@gmail.com>
2025-11-24 09:14:54 +0530

    Update ibd_app2.py

--- ibd_app2.py
+++ ibd_app2.py
@@ -7,8 +7,8 @@ from tensorflow.keras.models import load_model
 # Load trained models safely
 # -----------------------------
 try:
-    log_model = joblib.load("logistic_final.pkl")
-    scaler = joblib.load("scaler_final.pkl")
+    log_model = joblib.load("logistic_model_only.pkl")
+    scaler = joblib.load("scaler_final1.pkl")
 except FileNotFoundError as e:
     st.error(f"Error: Missing model file - {e}")
     st.stop()
